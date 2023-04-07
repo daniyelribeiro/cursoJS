408c033186e92e076ad57729bb353ea923375633
Daniel Ribeiro <daniyelribeiro@icloud.com>
2023-04-07 09:42:26 -0300

    Criei aula 4

---
+++ aula-11/ex-008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //condiçāo simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula-11/ex-009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log (`Se o país onde nasceu for: ${país}`)
+if (país == 'Cuba') {
+    console.log ('Voce é brasileiro')
+}
+else {
+    console.log ('Voce é estrangeiro')
+}
